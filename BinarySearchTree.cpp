c9fab55c2e8d6792c449991a2f90be099539d72c
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-26 18:36:57 +0700

    Tahap 12

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -69,7 +69,11 @@ class BinaryTree
         // Step 7: If the value in the data field of the new node is greater than or equal to the parent
         else if(x > parent->info)
         {
-            
+            // 7a: Make the right child of parent point to the new node
+            parent->rightchild = newNode;
+
+            // 7b: Exit
+            return;
         }
     }
 };
