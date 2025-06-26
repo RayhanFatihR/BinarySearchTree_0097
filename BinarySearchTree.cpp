3f0e613e2190d5456bf136dd80b688e1eaffb8b2
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-26 18:42:53 +0700

    Tahap 13

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -76,4 +76,15 @@ class BinaryTree
             return;
         }
     }
+
+    void search(int element, Node *&parent, Node *&currentNode)
+    {
+        // This function seacrhes the currentNode of the specified Node as well as the current Node of its parent
+        currentNode = ROOT; 
+        parent = nullptr;
+        while((currentNode != nullptr) && (currentNode->info != element))
+        {
+            
+        }
+    }
 };
