d4570c764c95117d1479668b72b2fbaa0f7be0eb
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-26 18:12:34 +0700

    Tahap 1

---
+++ BinarySearchTree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        int info;
+        Node *leftchild;
+        Node *rightchild;
+
+        //Constructor for the node class
+        Node()
+        {
+            leftchild = nullptr;  // Initialize left child to null
+            rightchild = nullptr; // Initialize right child to null
+        }
+};
+
+class BinaryTree
+{
+    public:
+        Node *ROOT;
+}
