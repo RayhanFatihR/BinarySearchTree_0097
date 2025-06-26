173d8ee6c2460cfd1b873ef897ac4493c9d89314
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-26 18:19:54 +0700

    Tahap 4

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -32,6 +32,12 @@ class BinaryTree
         cout << "Masukan nilai :";
         cin >> x;
 
+        // Step 1: Allocate memory for a new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data field of new node
+        newNode->info = x;
+
         
     }
 };
