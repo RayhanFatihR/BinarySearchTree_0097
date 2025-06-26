52ef88b7595d0111c89c2e57882a9d819462cd9e
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-26 18:20:47 +0700

    Tahap 5

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -38,6 +38,10 @@ class BinaryTree
         // Step 2: Assign value to the data field of new node
         newNode->info = x;
 
+        // Setep 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
         
     }
 };
